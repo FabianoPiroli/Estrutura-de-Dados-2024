f4242dd3c9e40645a0b8f0f007f57def08ce79c2
Fabiano <fabipiroli@live.com>
2024-09-11 21:32:40 -0300

    ListasGenericas

---
+++ AtividadeLivros/Livros.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtividadeLivros
+{
+    public class Book
+    {
+        public string? Nome { get; set; }
+        public string? Editora { get; set;}
+
+        public string? Autor { get; set;}
+
+        public int? NumeroPaginas { get; set; }
+
+
+    }
+}
---
+++ AtividadeLivros/Program.cs
@@ -0,0 +1,49 @@
+﻿using AtividadeLivros;
+
+
+
+List<Book> livro = new List<Book>();
+livro.Add(new Book(){
+    Nome = "A Arte da Guerra",
+    Editora = "Estação Liberdade",
+    Autor = "Sun Tzu",
+    NumeroPaginas = 160,
+});
+
+livro.Add(new Book(){
+    Nome = "IT, A coisa",
+    Editora = "Suma",
+    Autor = "Stephen King",
+    NumeroPaginas = 1104,
+});
+
+livro.Add(new Book(){
+    Nome = "48 Leis do Poder",
+    Editora = "Rocco",
+    Autor = "Robert Greene",
+    NumeroPaginas = 554,
+});
+
+livro.Add(new Book(){
+    Nome = "1984",
+    Editora = "Companhia das Letras",
+    Autor = "George Orwell",
+    NumeroPaginas = 416,
+});
+
+livro.Add(new Book(){
+    Nome = "Revolução dos Bichos",
+    Editora = "Companhia das Letras",
+    Autor = "George Orwell",
+    NumeroPaginas = 152,
+});
+
+Book MaisPaginas = livro.OrderByDescending(1 => 1.NumeroPaginas).First();
+{
+    Console.WriteLine("Livro com o maior número de paginas:");
+    Console.WriteLine($"Nome: {MaisPaginas.Nome}");
+    Console.WriteLine($"Editora: {MaisPaginas.Editora}");
+    Console.WriteLine($"Autor: {MaisPaginas.Autor}");
+    Console.WriteLine($"Numero de páginas: {MaisPaginas.NumeroPaginas}");
+
+}
