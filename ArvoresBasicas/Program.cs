73b39c0c951b655fd03bcfd6c5a8a749fc12371f
Fabiano Piroli <fabipiroli@live.com>
2024-10-27 20:40:15 -0300

    s

--- ArvoresBasicas/Program.cs
+++ ArvoresBasicas/Program.cs
@@ -4,17 +4,19 @@ Tree<int> tree = new Tree<int>();
 tree.Root = new Node<int>() {Data = 100};
 tree.Root.Children = 
     new List<Node<int>>{
-        new Node<int>() {Data = 50},
-        new Node<int>() {Data = 1},
-        new Node<int>() {Data = 150}
+        new Node<int>() {Data = 50, Parent = tree.Root},
+        new Node<int>() {Data = 1, Parent = tree.Root},
+        new Node<int>() {Data = 150, Parent = tree.Root}
     };
 
 
-Node<int> node12 = new Node<int>() {Data = 12};
-Node<int> node45 = new Node<int>() {Data = 45};
-Node<int> node21 = new Node<int>() {Data = 21};
+Node<int> node12 = new Node<int>() {Data = 12, Parent = tree.Root.Children[0]};
+Node<int> node45 = new Node<int>() {Data = 45, Parent = node12};
+Node<int> node21 = new Node<int>() {Data = 21, Parent = node12};
 
 node12.Children!.Add(node45!);
 node12.Children!.Add(node21!);
 
 tree.Root.Children[0].Children!.Add(node12);
+
+tree.PrintTree(tree.Root);
